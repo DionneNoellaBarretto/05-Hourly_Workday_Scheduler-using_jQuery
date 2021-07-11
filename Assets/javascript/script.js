92d0dcc7199068948f63977dc1ed7cd34bb634d1
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-07-11 17:31:30 -0400

    logging off for the day

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -8,15 +8,32 @@ f) add coffee  icon up top
 g) hide the schedule your workday button once its clicked
 ✅ h) text area placeholder 
 i) change the col size when using smaller screens  (https://www.geeksforgeeks.org/how-to-change-column-to-row-on-small-display-in-bootstrap-4/)
+j)  disable input to time blocks that are past
 */
+
+
+//Display the current date and time
+var timeDisplayed = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
+//console.log(timeDisplayed);
+// call currentTime every second
+setInterval(currentTime, 1000);
+
+// current time format
+function currentTime() {
+    // set the current time format
+    timeDisplayed = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
+    // Empty the current day element
+    $('#currentDay').empty();
+    // Display the current time
+    $('#currentDay').text(timeDisplayed);
+}
+// invoke the function to eliminate a 1s gap when the page loads
+currentTime();
 
 // returns the hour reading from the current moment in time - example: 18 for 18:XX hours, 2 for 2:xx hours
 var hourDisplayed = moment().format('H');
-// console.log(hourDisplayed);
+//console.log(hourDisplayed);
 
-// Add current date to the top of the page as defined here https://momentjs.com/
-$("#currentDay").text(moment().format('dddd, MMMM Do, YYYY' + " @ " + 'HH:mm:ss'));
- 
 // mapping the schedule your workday and reschedule button with an identifier
 var schedule = document.querySelector("#schedule");
 var reschedule = document.querySelector("#reschedule");
@@ -25,23 +42,26 @@ var reschedule = document.querySelector("#reschedule");
 // Add event listener to the schedule_your_day button
 schedule.addEventListener("click", scheduleDay);
 reschedule.addEventListener("click", rescheduleDay);
+
 // request user for start and end time
 function scheduleDay() { 
-var startHour = 0;
-startHour = prompt ("What hour do you start your day? \n\n Select a number between 1 and 24");
-if (startHour <1  || startHour > 24 || isNaN(startHour)) {
-    alert(`Invalid Entry!! \n\n Please click OK and click on 'Schedule_Your_Workday' button again to enter a valid number between 1 and 24 for the Work Day Scheduler to create appropriate hourly slots!`);
-    return;
-} else if (startHour >=1 && startHour < 12)
-    {
-alert(`You entered your start time as ${startHour} am!`);
-    } else if (startHour >= 13){
-        alert(`You entered your start start time as ${startHour}:00 hours in the evening!`);
+    var startHour = 0;
+    startHour = prompt ("What hour do you start your day? \n\n Select a number between 1 and 24");
+    if (startHour <1  || startHour > 24 || isNaN(startHour)) {
+        alert(`Invalid Entry!! \n\n Please click OK and click on 'Schedule_Your_Workday' button again to enter a valid number between 1 and 24 for the Work Day Scheduler to create appropriate hourly slots!`);
+        return;
+    } else if (startHour >=1 && startHour < 12)
+        {
+    alert(`You entered your start time as ${startHour} am!`);
+        } else if (startHour >= 13){
+            alert(`You entered your start start time as ${startHour}:00 hours in the evening!`);
+    
+        } else {
+            alert(`You entered your start time as ${startHour} o'clock'!`);
+        }
+        // console.log(startHour);
 
-    } else {
-        alert(`You entered your start time as ${startHour} o'clock'!`);
-    }
-    // console.log(startHour);
+
     // creates time block in the div container portion of the html file for a standard work hour slot of 8 hours (+1 hour lunch break) depending on the start time of the user)
 for (var i = startHour; i<= (Number(startHour)+9); i++) {
   //  console.log(i, Number(startHour));
@@ -59,10 +79,11 @@ for (var i = startHour; i<= (Number(startHour)+9); i++) {
    // places all these rows in the container class section of the html file
    $(".container").append(row);
            
-   // appends the right o'clock am/pm reading by calling the hourReading function and passing it an argument of i
-   hourReading(i);
-//also initializes the location of text to be saved in browser local storage
-   saveTasks();
+         
+    // appends the right o'clock am/pm reading by calling the hourReading function and passing it an argument of i
+    hourReading(i);
+    //also initializes the location of text to be saved in browser local storage
+       saveTasks();
 
    // Compare current hour with each of the time block's hours
 $(".col-md-8").each(function () {
@@ -78,7 +99,7 @@ $(".col-md-8").each(function () {
         $(this).addClass(" future");
     }
 });
-
+}
 }   
 
 function rescheduleDay() {
@@ -88,10 +109,11 @@ function rescheduleDay() {
     $(".reschedule").click(function (){
         $(".todo").val("");
         localStorage.clear();})
-        //recalls schedule your day function
-    reschedule.classList.add('hide');    
+        //recalls schedule your day function  
+          
 scheduleDay();
 }
+
 
 // Get the hour reading based on the i iteration from above and appends an am or pm depending on if the hour is before noon or after noon
 function hourReading(i) {
@@ -104,9 +126,7 @@ function hourReading(i) {
         hours = i + " o'clock";
     }
     return hours;
-    
 } 
-
 
 //Retrieves tasks inputted by the user thats stored in browser local storage
 function saveTasks() {
@@ -122,7 +142,6 @@ $(".saveBtn").click(function () {
     var inputAddress = $(this).siblings(".todo").attr("id");
     localStorage.setItem(inputAddress, userInputs);
 });
-
 
 // function to deletes user inputted text from the screen and from  browser local storage 
 $(".delBtn").click(function () {
@@ -141,6 +160,3 @@ $(".delBtn").click(function () {
         return;
     }
 });
-
-
-}
