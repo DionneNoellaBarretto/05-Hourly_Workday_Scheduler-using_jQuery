78c17b06e75a5197542f0084f8f5ca4f4ed3127b
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-07-13 04:12:29 -0400

    updated soln

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,22 +1,48 @@
 /*to do's 
-a) need to add a redo schedule and a clear all text button
+✅a) need to add the schedule and a back button
 b) see if the hours can be fixed when day starts in the evening 
 c) fix the color for present/future/past to work no matter say if user inputs 7am when actual time is 4pm
 d) add background image
-e) scale for table screen (intermediate)
+✅ e) scale for table screen (intermediate)
 f) add coffee  icon up top
-g) hide the schedule your workday button once its clicked
+✅ g) hide the schedule your workday button once its clicked
 ✅ h) text area placeholder 
-i) auto change the col size when using smaller screens  (https://www.geeksforgeeks.org/how-to-change-column-to-row-on-small-display-in-bootstrap-4/)
+✅ i) auto change the col size when using smaller screens  (https://www.geeksforgeeks.org/how-to-change-column-to-row-on-small-display-in-bootstrap-4/)
 j) disable input to time blocks that are past
 k) need to save to local storage and delete (need to make the buttons work)
 */
 
-//  as per https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie Note that we are setting `SameSite=None;` in this example because the example
-// needs to work cross-origin.
-// It is more common not to set the `SameSite` attribute, which results in the default,
-// and more secure, value of `SameSite=Lax;`
-document.cookie = "name=oeschger; SameSite=None; Secure";
+// back button being selected by id not class!
+var back = $('#back');
+// Immediately hide the back/scheduleReset button
+back.hide();
+// scheduleReset.hide();
+var instructional_alert = $(".alert")
+
+// //reset existing reschedule 
+// var scheduleReset = $('#scheduleReset');
+// //click event listener
+// scheduleReset.on ("click",resetSchedule);
+// //function assigned to a variable
+// var resetSchedule = function(event){
+//     event.preventDefault();
+// if (!userText){
+//     console.log("There's nothing to clear, You need to fill up your day's schedule!");
+//     return;
+// }
+// userText.val('');
+// };
+
+// returns the hour reading from the current moment in time - example: 18 for 18:XX hours, 2 for 2:xx hours
+var hourDisplayed = moment().format('H');
+//console.log(hourDisplayed);
+
+// mapping the schedule your workday and save button with identifiers
+var schedule = document.querySelector("#schedule");
+// console.log(schedule);
+
+// Add event listener to the schedule_your_day button
+schedule.addEventListener("click", scheduleDay);
 
 //Display the current date and time
 var timeDisplayed = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
@@ -36,17 +62,6 @@ function currentTime() {
 // invoke the function to eliminate a 1s gap when the page loads
 currentTime();
 
-// returns the hour reading from the current moment in time - example: 18 for 18:XX hours, 2 for 2:xx hours
-var hourDisplayed = moment().format('H');
-//console.log(hourDisplayed);
-
-// mapping the schedule your workday and save button with identifiers
-var schedule = document.querySelector("#schedule");
-// console.log(schedule);
-
-// Add event listener to the schedule_your_day button
-schedule.addEventListener("click", scheduleDay);
-
 // request user for start and end time
 function scheduleDay() { 
     var startHour = 0;
@@ -64,25 +79,28 @@ function scheduleDay() {
             alert(`You entered your start time as ${startHour} o'clock'!`);
         }
         // console.log(startHour);
-//time slot row variable
-        var row = $('<div class="row">');
+//time slot row variable that uses row class to create row in the container 
+// Change the alignment of flex item to center with justify-content-center      
+var row = $('<div class="w-100 row justify-content-center">');
     // creates time block in the div container portion of the html file for a standard work hour slot of 8 hours (+1 hour lunch break) depending on the start time of the user)
 var timeBlocks = $('.container');
 for (var i = startHour; i<= (Number(startHour)+9); i++) {
     //  console.log(i, Number(startHour));
 
 // bootstrap grid system concept for sizing the widths : ref --> https://www.w3schools.com/bootstrap/bootstrap_grid_system.asp
-    var hour = $('<div class="hour col-1">');
+var hour = $('<div class="hour col-md-1">');
 //console.log(hour);
 if (i < 12) {
-    hour.text(i + " :00 AM");
+    // Add text to the hour element defined above
+    hour.text(i + ":00 AM");
 } else if(i > 12){
-    hour.text((i-12) + " :00 PM");
+    hour.text((i-12) + ":00 PM");
 } else if ((i == 12 || i ==24)){
     hour.text(i + " o'Clock");
 }
-    // learnt how to add a text area placeholder https://www.w3schools.com/tags/att_textarea_placeholder.asp
-    var userText = $('<textarea placeholder="Enter your todo task here.." class="text ">').addClass('todo col-8');
+// Adds the class `todo and col-md-8` to the user text element
+    // learnt how to add a text area placeholder https://www.w3schools.com/tags/att_textarea_placeholder.asp + // Uses col-*-* class for viewing to adjust on different screens..after trying several permutations and combinations settling for the col-md-* class i've chosen these values to appear full-size on small devices and half-size on medium or larger devices -->
+    var userText = $('<textarea placeholder="Enter your todo task here.." class="text ">').addClass('todo col-md-8');
             // check to see if there is saved data to pull into the time blocks
 if(localStorage.getItem("cache") === null){
     // console.log(todoData);
@@ -104,23 +122,40 @@ if (i < hourDisplayed) {
     userText.addClass(" future")
 }
 
+// https://getbootstrap.com/docs/4.0/components/buttons/ 
     // flex box container concept https://getbootstrap.com/docs/4.4/utilities/flex/, save icon from font awesome site https://fontawesome.com/v5.15/icons/save?style=regular , trash icon https://fontawesome.com/v5.15/icons/trash?style=solid
-    var saveBtn = $('<button id="save">').addClass(" col-md-1 btn save d-flex justify-content-center align-items-center fas fa-save");
-    // var delBtn = $('<button>').addClass(" col-md-1 btn erase d-flex justify-content-center align-items-center fas fa-trash fa-w-14 fa-spin fa-lg");
-   // joins up all the sections defined above to form a schedule for the user in this particular order
-   row.append(hour, userText,saveBtn);
-   // places all these rows in the container class section of the html file
+    var saveBtn = $('<button id="save">').addClass(" disabled col-md-1 btn-block btn save d-flex justify-content-center align-items-center fas fa-save");
+    var delBtn = $('<button>').addClass(" disabled col-md-1 erase btn btn-block d-flex justify-content-center align-items-center fas fa-trash");
+   // joins up all the (sections) defined above to form a hourly schedule grid for the user in this particular order for a given row 
+   row.append(hour, userText,saveBtn,delBtn);
+   // places all these rows in the container class section of the html file by appending to the page
    timeBlocks.append(row);
-}};
+}
+   //displays the back/reset Schedule button once the schedule is created
+   back.show();
+scheduleReset.show();
+   //hides the instructional alert up top
+instructional_alert.hide();
+
+};
 
 
 // put event data in local storage when clicking save button.
 $('.save').on('click',function(){
+    saveBtn.addClass(" active");
 
     // variable to hold todo data that user enters 
 var todo =[];
+var todoJSON = JSON.stringify(todo);
     $('.todo').each(function(){
-        todo.push(this.value);
+        todo.push($(this).val());
     });
-    localStorage.setItem("cache", JSON.stringify(todo));
+    localStorage.setItem("cache", cache.value);
+    localStorage.setItem(("todo"), todoJSON);
 })
+
+
+
+
+
+
